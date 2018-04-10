8337c403a442e6890c481d773db319cf7974b699
elishakay <kramer1346@gmail.com>
2018-04-10 19:47:30 +0300

    Revert "looping through questions on browser and printing in node cli"
    This reverts commit ab7960d841f5434790f804ee018e09a6de9faa2e.

--- test/specs/WithoutWdioClient.js
+++ test/specs/WithoutWdioClient.js
@@ -26,7 +26,7 @@ LoginPage.loginClick();
     begin.click();
 
 
-    browser.url('http://tqen.mot.gov.il/know-your-vehicle?start=80') //Navigates to google.com
+    browser.url('http://www.isradrive.com') //Navigates to google.com
     .getTitle().then(function(title) { // Gets the title of the page.
         console.log('Title was: ' + title); // Prints the title of the page.
     });
