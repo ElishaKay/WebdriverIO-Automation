c64952c443c1c57e1e3c40446163f0ed3e12a39c
elishakay <kramer1346@gmail.com>
2018-04-13 14:17:22 +0300

    successfully sends question and answers to the koalacms server via jquery api

--- test/specs/saveQuestions.js
+++ test/specs/saveQuestions.js
@@ -31,6 +31,71 @@ function wait(ms){
 wait(15000);
 
 var result = browser.execute(function() {
+
+	// Inject jquery version with http support
+		(function() {
+	  var el=document.createElement('div'),
+	   b=document.getElementsByTagName('body')[0];
+	   otherlib=false,
+	   msg='';
+	  el.style.position='fixed';
+	  el.style.height='32px';
+	  el.style.width='220px';
+	  el.style.marginLeft='-110px';
+	  el.style.top='0';
+	  el.style.left='50%';
+	  el.style.padding='5px 10px';
+	  el.style.zIndex = 1001;
+	  el.style.fontSize='12px';
+	  el.style.color='#222';
+	  el.style.backgroundColor='#f99';
+	  
+	  // more or less stolen form jquery core and adapted by paul irish
+	  function getScript(url,success){
+	 var script=document.createElement('script');
+	 script.src=url;
+	 var head=document.getElementsByTagName('head')[0],
+	  done=false;
+	 // Attach handlers for all browsers
+	 script.onload=script.onreadystatechange = function(){
+	   if ( !done && (!this.readyState
+	     || this.readyState == 'loaded'
+	     || this.readyState == 'complete') ) {
+	  done=true;
+	  success();
+	  script.onload = script.onreadystatechange = null;
+	  head.removeChild(script);
+	   }
+	 };
+	 head.appendChild(script);
+	  }
+	  getScript('https://code.jquery.com/jquery-latest.min.js',function() {
+	 if (typeof jQuery=='undefined') {
+	   msg='Sorry, but jQuery wasn\'t able to load';
+	 } else {
+	   msg='This page is now jQuerified with v' + jQuery.fn.jquery;
+	   if (otherlib) {msg+=' and noConflict(). Use $jq(), not $().';}
+	 }
+	 return showMsg();
+	  });
+	  function showMsg() {
+	 el.innerHTML=msg;
+	 b.appendChild(el);
+	 window.setTimeout(function() {
+	   if (typeof jQuery=='undefined') {
+	  b.removeChild(el);
+	   } else {
+	  jQuery(el).fadeOut('slow',function() {
+	    jQuery(this).remove();
+	  });
+	  if (otherlib) {
+	    $jq=jQuery.noConflict();
+	  }
+	   }
+	 } ,2500);
+	  }
+	})();
+
     var saveQLoop = setInterval(function(){
     var list = document.getElementsByClassName("jcepopup");
     console.log('this is the list array', list);
@@ -60,6 +125,11 @@ var result = browser.execute(function() {
          	//   
 		}
 	console.log('this is the questions and answer array', newList);
+	$.post( "http://localhost/api/drivingquestions", 
+		  { 'theQuestions': newList },
+		function(   data ) {
+		  console.log(data);
+	});
 	clearInterval(saveQLoop);
     // browser context - you may not access client or console
     }, 5000);
